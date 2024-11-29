0b1db2187552b556f4cf668b4ceb25b8189d5e68
Jawad Chowdhury <jawadchy15@gmail.com>
2024-11-29 17:33:13 -0500

    chore: favicon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -137,7 +137,8 @@ export const Navbar = () => {
               />
               <div className="absolute inset-0 flex items-center pointer-events-none px-4">
                 {searchQuery ? (
-                  <span className="dark:text-white">{searchQuery}</span>
+                  <>
+                  </>
                 ) : 
                 (
                   <>
