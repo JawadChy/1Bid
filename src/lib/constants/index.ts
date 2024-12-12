6be378317035880c0aff30602fd5d33b207926be
Darren Ling <darrenl6131@gmail.com>
2024-12-12 02:20:01 -0500

    Added to protected paths

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,10 @@
-export const protectedPaths = ["/private"]
+export const protectedPaths = [
+    "/private", 
+    "/mylistings", 
+    "/superapplication", 
+    "/settings", 
+    "/wallet", 
+    "createlisting"
+]
 {/* TODO: change auth to (auth) and export authPaths */}
 // export const authPaths = ["/signup", "/signin", "/confirm"]
